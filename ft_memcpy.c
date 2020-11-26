f3bfb3ccbcdf62595ff7ad907de806f247a55630
Kokou HELEGBE <khelegbe@e3r10p5.42.fr>
2020-11-26 14:16:20 +0100

    protection added memcpy

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -6,7 +6,7 @@
 /*   By: khelegbe <khelegbe@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/22 21:07:53 by khelegbe          #+#    #+#             */
-/*   Updated: 2020/11/26 14:04:53 by khelegbe         ###   ########.fr       */
+/*   Updated: 2020/11/26 14:13:51 by khelegbe         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,8 +18,8 @@ void	*ft_memcpy(void *dest, const void *src, size_t n)
 	size_t		i;
 	char		*s;
 
-	if (dest == 0 || src == 0)
-		return (0);
+	if (dest == src || !n)
+		return (dest);
 	i = 0;
 	d = dest;
 	s = (char *)src;
