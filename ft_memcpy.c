3cffa907b60557ce89239930114183a045671975
HKSerge <kserge.helegbe@gmail.com>
2020-11-26 14:06:19 +0100

    memcpy corrected

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -6,7 +6,7 @@
 /*   By: khelegbe <khelegbe@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/22 21:07:53 by khelegbe          #+#    #+#             */
-/*   Updated: 2020/11/26 12:22:26 by khelegbe         ###   ########.fr       */
+/*   Updated: 2020/11/26 14:04:53 by khelegbe         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,6 +18,8 @@ void	*ft_memcpy(void *dest, const void *src, size_t n)
 	size_t		i;
 	char		*s;
 
+	if (dest == 0 || src == 0)
+		return (0);
 	i = 0;
 	d = dest;
 	s = (char *)src;
