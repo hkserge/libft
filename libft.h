4d9394b0a13a8c6c0a2363fad5d5ac309b8892ce
HKSerge <kserge.helegbe@gmail.com>
2020-11-30 21:44:36 +0100

    memchr done

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: khelegbe <khelegbe@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/19 13:40:59 by khelegbe          #+#    #+#             */
-/*   Updated: 2020/11/27 20:16:52 by khelegbe         ###   ########.fr       */
+/*   Updated: 2020/11/30 18:20:08 by khelegbe         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -37,5 +37,6 @@ void				ft_bzero(void *s, size_t n);
 void				*ft_memcpy(void *dest, const void *src, size_t n);
 void				*ft_memccpy(void *dest, const void *src, int c, size_t n);
 void				*ft_memmove(void *dest, const void *src, size_t n);
+void				*ft_memchr(const void *s, int c, size_t n);
 
 #endif
