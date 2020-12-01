cc67ed4c62384b362aa37bbcb855be61c99f87d4
HKSerge <kserge.helegbe@gmail.com>
2020-12-01 16:15:18 +0100

    strrchr done

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: khelegbe <khelegbe@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/19 13:40:59 by khelegbe          #+#    #+#             */
-/*   Updated: 2020/12/01 14:14:32 by khelegbe         ###   ########.fr       */
+/*   Updated: 2020/12/01 15:15:17 by khelegbe         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -39,5 +39,6 @@ void				*ft_memccpy(void *dest, const void *src, int c, size_t n);
 void				*ft_memmove(void *dest, const void *src, size_t n);
 void				*ft_memchr(const void *s, int c, size_t n);
 int					ft_memcmp(const void *s1, const void *s2, size_t n);
+char				*ft_strrchr(const char *s, int c);
 
 #endif
