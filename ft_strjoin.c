20f56af817c77c34619a6a47a38aa4226498a5ac
HKSerge <kserge.helegbe@gmail.com>
2020-12-11 17:22:27 +0100

    strjoin

--- ft_strjoin.c
+++ ft_strjoin.c
@@ -1,1 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strjoin.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: khelegbe <khelegbe@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/12/11 17:10:18 by khelegbe          #+#    #+#             */
+/*   Updated: 2020/12/11 17:21:42 by khelegbe         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strjoin(char const *s1, char const *s2)
+{
+	unsigned int	len;
+	char			*str;
+
+	len = ft_strlen(s1) + ft_strlen(s2);
+	if (!(str = malloc(sizeof(char) * len)))
+		return (0);
+	ft_strlcpy(str, s1, ft_strlen(s1) + 1);
+	ft_strlcpy(str + ft_strlen(s1), s2, ft_strlen(s2) + 1);
+	return (str);
 }
