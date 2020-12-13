1fbc786df16d61193e285babe1c13e0873cd2ac2
HKSerge <kserge.helegbe@gmail.com>
2020-12-13 19:14:12 +0100

    strtrim

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -6,7 +6,7 @@
 /*   By: khelegbe <khelegbe@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/12/07 17:15:00 by khelegbe          #+#    #+#             */
-/*   Updated: 2020/12/13 18:48:03 by khelegbe         ###   ########.fr       */
+/*   Updated: 2020/12/13 19:10:19 by khelegbe         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,16 +25,14 @@ static int	ft_check_set(const char c, const char *set)
 
 char		*ft_strtrim(char const *s1, char const *set)
 {
-	int	start;
-	int	end;
+	size_t	end;
 
 	if (!s1 || !set)
 		return (0);
-	start = 0;
-	end = ft_strlen(s1) - 1;
-	while (ft_check_set(s1[start], set))
-		start++;
-	while (ft_check_set(s1[end], set))
+	while (*s1 && ft_strchr(set, *s1))
+		s1++;
+	end = ft_strlen(s1);
+	while (end && ft_strchr(set, s1[end]))
 		end--;
-	return (ft_substr(s1, start, end));
+	return (ft_substr((char*)s1, 0, end + 1));
 }
