da721c3b21b27f2ecb8a450306df53dec4d68e91
HKSerge <kserge.helegbe@gmail.com>
2020-12-15 17:18:20 +0100

    strmapi

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: khelegbe <khelegbe@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/19 13:40:59 by khelegbe          #+#    #+#             */
-/*   Updated: 2020/12/13 15:12:14 by khelegbe         ###   ########.fr       */
+/*   Updated: 2020/12/15 17:05:11 by khelegbe         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -55,5 +55,6 @@ char				*ft_strtrim(char const *s1, char const *set);
 char				*ft_substr(char const *s, unsigned int start, size_t len);
 char				*ft_strjoin(char const *s1, char const *s2);
 char				**ft_split(char const *s, char c);
+char				*ft_strmapi(char const *s, char (*f)(unsigned int, char));
 
 #endif
